0ab683df768dda85f7518933d1889aa04846ba1a
Zhang Yuzheng <me@zhanyuzheng.com>
2018-08-06 21:48:02 +0800

    init: feat: add env init and python header template
    Signed-off-by: Zhang Yuzheng <me@zhanyuzheng.com>

---
+++ init.sh
@@ -0,0 +1,13 @@
+# Install miniconda or anaconda
+pip install setuptools pipenv ipython python-dev python3-dev bs4
+conda create -n py27 python=2.7
+
+# SourceCodePro
+mkdir ~/.fonts
+cd ~/.fonts
+wget https://github.com/adobe-fonts/source-code-pro/releases/download/variable-fonts/SourceCodeVariable-Italic.ttf
+wget https://github.com/adobe-fonts/source-code-pro/releases/download/variable-fonts/SourceCodeVariable-Roman.ttf
+fc-cache
+
+sudo apt-get install git tig build-essential cmake python-dev python3-dev
+
---
+++ python_header.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+ 
+# -*- encoding: utf-8 -*-
+ 
+'''
+@Author  :   {YourName}
+ 
+@License :   (C) Copyright 2013-2017, {YourCompany}
+ 
+@Contact :   {YourEmail}
+ 
+@Software:   ${PRODUCT_NAME}
+ 
+@File    :   ${NAME}.py
+ 
+@Time    :   ${DATE} ${TIME}
+ 
+@Desc    :
+ 
+'''
+ 
+import...
+ 
+ 
+if __name__=='__main__':
+    pass
